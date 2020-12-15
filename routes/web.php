44863ed5a8745796fda27224c5303b1df2c55181
Marko Lisica <titelskibreg@gmail.com>
2020-12-15 17:29:49 +0100

    Debug #1

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Response;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/env', function () {
+    return response()->json($_ENV);
 });
