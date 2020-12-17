fabe3e13f5b3e6f100e503e7cb5e4806fc6a8acc
Marko Lisica <titelskibreg@gmail.com>
2020-12-17 21:00:57 +0100

    Working example Alpha #0 fix

---
+++ aws/src/vendor/async-aws/core/src/Exception/Http/HttpExceptionTrait.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace AsyncAws\Core\Exception\Http;
+
+use AsyncAws\Core\AwsError\AwsError;
+use AsyncAws\Core\AwsError\AwsErrorFactory;
+use AsyncAws\Core\Exception\UnparsableResponse;
+use Symfony\Contracts\HttpClient\ResponseInterface;
+
+/**
+ * @author Nicolas Grekas <p@tchwork.com>
+ * @author Tobias Nyholm <tobias.nyholm@gmail.com>
+ * @author Jérémy Derussé <jeremy@derusse.com>
+ *
+ * @internal
+ */
+trait HttpExceptionTrait
+{
+    /**
+     * @var ResponseInterface
+     */
+    private $response;
+
+    /**
+     * @var ?AwsError
+     */
+    private $awsError;
+
+    public function __construct(ResponseInterface $response)
+    {
+        $this->response = $response;
+        /** @var int $code */
+        $code = $response->getInfo('http_code');
+        /** @var string $url */
+        $url = $response->getInfo('url');
+
+        try {
+            $this->awsError = AwsErrorFactory::createFromResponse($response);
+        } catch (UnparsableResponse $e) {
+            // Ignore parsing error
+        }
+
+        $message = sprintf('HTTP %d returned for "%s".', $code, $url);
+        if (null !== $this->awsError) {
+            $message .= <<<TEXT
+
+
+Code:    {$this->awsError->getCode()}
+Message: {$this->awsError->getMessage()}
+Type:    {$this->awsError->getType()}
+Detail:  {$this->awsError->getDetail()}
+
+TEXT;
+        }
+
+        parent::__construct($message, $code);
+    }
+
+    public function getResponse(): ResponseInterface
+    {
+        return $this->response;
+    }
+
+    public function getAwsCode(): ?string
+    {
+        return $this->awsError ? $this->awsError->getCode() : null;
+    }
+
+    public function getAwsType(): ?string
+    {
+        return $this->awsError ? $this->awsError->getType() : null;
+    }
+
+    public function getAwsMessage(): ?string
+    {
+        return $this->awsError ? $this->awsError->getMessage() : null;
+    }
+
+    public function getAwsDetail(): ?string
+    {
+        return $this->awsError ? $this->awsError->getDetail() : null;
+    }
+}
---
+++ aws/src/vendor/async-aws/core/src/HttpClient/AwsRetryStrategy.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace AsyncAws\Core\HttpClient;
+
+use AsyncAws\Core\AwsError\AwsErrorFactory;
+use AsyncAws\Core\Exception\UnparsableResponse;
+use Symfony\Component\HttpClient\Response\AsyncContext;
+use Symfony\Component\HttpClient\Retry\GenericRetryStrategy;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+/**
+ * @author Jérémy Derussé <jeremy@derusse.com>
+ */
+class AwsRetryStrategy extends GenericRetryStrategy
+{
+    public const DEFAULT_RETRY_STATUS_CODES = [0, 423, 425, 429, 500, 502, 503, 504, 507, 510];
+
+    // Override Symfony default options for a better integration of AWS servers.
+    public function __construct(array $statusCodes = self::DEFAULT_RETRY_STATUS_CODES, int $delayMs = 1000, float $multiplier = 2.0, int $maxDelayMs = 0, float $jitter = 0.1)
+    {
+        parent::__construct($statusCodes, $delayMs, $multiplier, $maxDelayMs, $jitter);
+    }
+
+    public function shouldRetry(AsyncContext $context, ?string $responseContent, ?TransportExceptionInterface $exception): ?bool
+    {
+        if (parent::shouldRetry($context, $responseContent, $exception)) {
+            return true;
+        }
+
+        if (400 !== $context->getStatusCode()) {
+            return false;
+        }
+
+        if (null === $responseContent) {
+            return null; // null mean no decision taken and need to be called again with the body
+        }
+
+        try {
+            $error = AwsErrorFactory::createFromContent($responseContent, $context->getHeaders());
+        } catch (UnparsableResponse $e) {
+            return false;
+        }
+
+        return \in_array($error->getCode(), [
+            'RequestLimitExceeded',
+            'Throttling',
+            'ThrottlingException',
+            'ThrottledException',
+            'LimitExceededException',
+            'PriorRequestNotComplete',
+            'ProvisionedThroughputExceededException',
+            'RequestThrottled',
+            'SlowDown',
+            'BandwidthLimitExceeded',
+            'RequestThrottledException',
+            'RetryableThrottlingException',
+            'TooManyRequestsException',
+            'IDPCommunicationError',
+            'EC2ThrottledException',
+            'TransactionInProgressException',
+        ], true);
+    }
+}
